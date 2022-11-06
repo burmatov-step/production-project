4202083cbecb84daf995c125418ac514b1acadae
sburmatov <58148434+burmatov-step@users.noreply.github.com>
2022-11-06 14:17:51 +0500

    add redux

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,9 @@
+import { counterReducer } from "./modal/slice/counterSlice"
+import { Counter } from "./ui/Counter"
+import type { CounterSchema } from "./modal/types/counterSchema"
+
+export {
+    counterReducer,
+    Counter,
+    CounterSchema,
+}
---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,32 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { Button } from 'shared/ui/Button/Button'
+import { getCounterValue } from "../modal/selectors/getCounterValue/getCounterValue";
+import { counterActions } from "../modal/slice/counterSlice"
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue)
+    const increment = () =>{
+        dispatch(counterActions.incremented())
+    }
+
+    const decrement = () =>{
+        dispatch(counterActions.decremented())
+    }
+
+    return (
+        <div>
+            <h1 data-testId="value-title" >{counterValue}</h1>
+            <Button 
+                onClick={increment}
+                data-testId="increment-btn" >
+                increment
+            </Button>
+            <Button 
+                onClick={decrement}
+                data-testId="decrement-btn">
+                decrement
+            </Button>
+        </div>
+    )
+}
